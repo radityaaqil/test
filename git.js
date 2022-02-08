b67c16d54d71862111fb116119311e1c59681a34
radityaaqil <radityaaqil@gmail.com>
2022-02-08 10:53:39 +0700

    AA

--- git.js
+++ git.js
@@ -6,6 +6,8 @@
 //b. modified : file sudah masuk di git tetapi sedang dirubah, sudah dirubah atau berbeda dengan yang ada di commit
 //c. stage : dari untracked masuk ke fase stage bisa aja berubah menjadi added atau modified atau pada fase ini kita mendifinisikan file apa yang akan dimasukkan ke commit
 //d. commit : commit adalah checkpoint di git
-    //git commit -m ""
+    //git commit -m "" (sebelum commit pasti git n)
 //e. git add : memasukkan file yang dimau ke fase stage contoh
     //git add . (titik (.) artinya semua file yang ada di folder tempat terminal berada atau jika mau satu file saja maka tulis git add namafile, bisa lebih dari satu pisahkan dengan)
+
+//disini
