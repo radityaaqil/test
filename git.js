43d22c819d494259da9d592f8e7af69ea2ae48f1
radityaaqil <radityaaqil@gmail.com>
2022-02-08 10:43:50 +0700

    sg genah

---
+++ git.js
@@ -0,0 +1,11 @@
+//commit adalah checkpoint
+//gitBash itu adalah base terminal ngikutin linux untuk windows
+
+//Fase di git ada :
+//a. untracked : pada saat file aru dibuat dan belum masuk sama sekali di git
+//b. modified : file sudah masuk di git tetapi sedang dirubah, sudah dirubah atau berbeda dengan yang ada di commit
+//c. stage : dari untracked masuk ke fase stage bisa aja berubah menjadi added atau modified atau pada fase ini kita mendifinisikan file apa yang akan dimasukkan ke commit
+//d. commit : commit adalah checkpoint di git
+    //git commit -m ""
+//e. git add : memasukkan file yang dimau ke fase stage contoh
+    //git add . (titik (.) artinya semua file yang ada di folder tempat terminal berada atau jika mau satu file saja maka tulis git add namafile, bisa lebih dari satu pisahkan dengan)
